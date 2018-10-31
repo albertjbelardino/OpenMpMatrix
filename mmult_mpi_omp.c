62068307e7476e7133522ef38a8cb436c4547f59
Albert Joseph Belardino <tug83326@chinook.cis.temple.edu>
2018-10-31 12:46:31 -0400

    added pseudocode in appropriate spots in mpi_omp.c file

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -26,6 +26,7 @@ int main(int argc, char* argv[])
   double starttime, endtime;
   MPI_Status status;
   /* insert other global variables here */
+ 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -34,11 +35,31 @@ int main(int argc, char* argv[])
     ncols = nrows;
     if (myid == 0) {
       // Master Code goes here
+		/*
+			first part
+      //start from here
+for each row in A
+   //this following is the matrix times vector program
+   broadcast row a to every slave
+   for each column vector in B
+       send column vector b to some slave process 
+		*/
       aa = gen_matrix(nrows, ncols);
       bb = gen_matrix(ncols, nrows);
       cc1 = malloc(sizeof(double) * nrows * nrows); 
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
+	/*
+		second part
+               //master code
+for i = 0: i < n^2: i++
+   receive result
+   put in proper index 
+   if there are more rows/columns to send
+       send more
+   else 
+       stop
+	*/
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
       cc2  = malloc(sizeof(double) * nrows * nrows);
@@ -46,6 +67,15 @@ int main(int argc, char* argv[])
       compare_matrices(cc2, cc1, nrows, nrows);
     } else {
       // Slave Code goes here
+	/*
+		third part
+while(1)
+  receive row and column
+  if stop
+    break
+  compute dot product
+  send back to master
+	*/
     }
   } else {
     fprintf(stderr, "Usage matrix_times_vector <size>\n");
