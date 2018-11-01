f234a4bde649c9f818862ee6f8a35f33b68d9392
Binh Nguyen <tug21976@temple.edu>
2018-11-01 15:21:06 -0400

    Finish first part
    Broadcast each row of aa to every slave, then send to each slave process a
    column vector of bb.

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -7,7 +7,7 @@
 
 double* gen_matrix(int n, int m);
 int mmult(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
-void compare_matrix(double *a, double *b, int nRows, int nCols);
+void compare_matrices(double *a, double *b, int nRows, int nCols);
 
 /** 
     Program to multiply a matrix times a matrix using both
@@ -22,23 +22,39 @@ int main(int argc, char* argv[])
   double *bb;	/* the B matrix */
   double *cc1;	/* A x B computed using the omp-mpi code you write */
   double *cc2;	/* A x B computed using the conventional algorithm */
-  int myid, numprocs;
+  double *buffer;
+  int myid, master, numprocs;
   double starttime, endtime;
   MPI_Status status;
   /* insert other global variables here */
+  int i, j, k, numsent, sender;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   if (argc > 1) {
     nrows = atoi(argv[1]);
     ncols = nrows;
-    if (myid == 0) {
+    master = 0;
+    if (myid == master) {
       // Master Code goes here
       aa = gen_matrix(nrows, ncols);
       bb = gen_matrix(ncols, nrows);
       cc1 = malloc(sizeof(double) * nrows * nrows); 
+      buffer = malloc(sizeof(double) * ncols);
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
+      numsent = 0;
+      for (i = 0; i < nrows; i++) {
+        for (j = 0; j < ncols; j++)
+          buffer[j] = aa[i*ncols + j];
+        MPI_Bcast(buffer, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
+        for (j = 0; j < ncols; j++) {
+          for (k = 0; k < nrows; k++)
+            buffer[k] = bb[j + k*nrows];
+          BPI_Send(buffer, nrows, MPI_DOUBLE, i*ncols + j, i*ncols + j, MPI_COMM_WORLD);
+          numsent++;
+        }
+      }
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
       cc2  = malloc(sizeof(double) * nrows * nrows);
