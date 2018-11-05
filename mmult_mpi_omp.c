fe27d84d0a5ece09b422a500de4f6c9813ac7725
Binh Nguyen <tug21976@temple.edu>
2018-11-05 16:42:17 -0500

    Finish second and third parts (IN PROGRESS)
    Implement second and third parts in TODO_v1.txt according to
    matrix_times_vector.c, but not working yet.

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -22,12 +22,15 @@ int main(int argc, char* argv[])
   double *bb;	/* the B matrix */
   double *cc1;	/* A x B computed using the omp-mpi code you write */
   double *cc2;	/* A x B computed using the conventional algorithm */
-  double *buffer;
+  double *a;
+  double *buffer, ans;
   int myid, master, numprocs;
   double starttime, endtime;
   MPI_Status status;
   /* insert other global variables here */
   int i, j, k, numsent, sender;
+  int anstype, entry;
+  int cur_row, cur_col;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -44,15 +47,40 @@ int main(int argc, char* argv[])
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
       numsent = 0;
-      for (i = 0; i < nrows; i++) {
-        for (j = 0; j < ncols; j++)
+      for (i = 0; i < min((numprocs-1) / ncols, nrows); i++) {
+        for (j = 0; j < min((numprocs-1) % ncols, ncols); j++)
           buffer[j] = aa[i*ncols + j];
         MPI_Bcast(buffer, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
-        for (j = 0; j < ncols; j++) {
+        for (j = 0; j < min((numprocs-1) % ncols, ncols); j++) {
           for (k = 0; k < nrows; k++)
             buffer[k] = bb[j + k*nrows];
-          BPI_Send(buffer, nrows, MPI_DOUBLE, i*ncols + j, i*ncols + j, MPI_COMM_WORLD);
+          MPI_Send(buffer, nrows, MPI_DOUBLE, i*ncols + j, i*ncols + j, MPI_COMM_WORLD);
           numsent++;
+        }
+      }
+      cur_row = 0;
+      cur_col = 0;
+      for (i = 0; i < nrows * ncols; i++) {
+        MPI_Recv(&ans, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        sender = status.MPI_SOURCE;
+        anstype = status.MPI_TAG;
+        cc1[anstype - 1] = ans;
+        if (numsent < nrows * ncols) {
+          if (numsent % ncols == 0) {
+            cur_row++;
+            cur_col = 0;
+            for (j = 0; j < ncols; j++)
+              buffer[j] = aa[cur_row*ncols + j];
+            MPI_Bcast(buffer, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
+          } else {
+            cur_col++;
+          }
+          for (j = 0; j < nrows; j++)
+            buffer[j] = bb[cur_col + cur_row*nrows];
+          MPI_Send(buffer, nrows, MPI_DOUBLE, cur_row*ncols + cur_col, cur_row*ncols + cur_col, MPI_COMM_WORLD);
+          numsent++;
+        } else {
+          MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
         }
       }
       endtime = MPI_Wtime();
@@ -62,6 +90,19 @@ int main(int argc, char* argv[])
       compare_matrices(cc2, cc1, nrows, nrows);
     } else {
       // Slave Code goes here
+      MPI_Bcast(a, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
+      if (myid <= nrows * ncols) {
+        while (1) {
+          MPI_Recv(buffer, ncols, MPI_DOUBLE, master, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+          if (status.MPI_TAG == 0)
+            break;
+          entry = status.MPI_TAG;
+          ans = 0.0;
+          for (j = 0; j < ncols; j++)
+            ans += a[j] * buffer[j];
+          MPI_Send(&ans, 1, MPI_DOUBLE, master, entry, MPI_COMM_WORLD);
+        }
+      }
     }
   } else {
     fprintf(stderr, "Usage matrix_times_vector <size>\n");
