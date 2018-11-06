5275d09c11160bccf9eb020f41041d812f7793ec
Binh Nguyen <tug21976@temple.edu>
2018-11-05 20:53:42 -0500

    Fix last commit (IN PROGRESS)
    Now everything works perfectly, but only when the number of worker processes is
    less than the number of columns (since implementation currently only handles
    this case). Later revisions will handle this issue completely.

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -37,29 +37,29 @@ int main(int argc, char* argv[])
   if (argc > 1) {
     nrows = atoi(argv[1]);
     ncols = nrows;
+    buffer = malloc(sizeof(double) * ncols);
     master = 0;
     if (myid == master) {
       // Master Code goes here
       aa = gen_matrix(nrows, ncols);
       bb = gen_matrix(ncols, nrows);
       cc1 = malloc(sizeof(double) * nrows * nrows); 
-      buffer = malloc(sizeof(double) * ncols);
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
       numsent = 0;
-      for (i = 0; i < min((numprocs-1) / ncols, nrows); i++) {
-        for (j = 0; j < min((numprocs-1) % ncols, ncols); j++)
+      for (i = 0; i < 1; i++) {
+        for (j = 0; j < ncols; j++)
           buffer[j] = aa[i*ncols + j];
         MPI_Bcast(buffer, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
-        for (j = 0; j < min((numprocs-1) % ncols, ncols); j++) {
+        for (j = 0; j < numprocs - 1; j++) {
           for (k = 0; k < nrows; k++)
             buffer[k] = bb[j + k*nrows];
-          MPI_Send(buffer, nrows, MPI_DOUBLE, i*ncols + j, i*ncols + j, MPI_COMM_WORLD);
+          MPI_Send(buffer, nrows, MPI_DOUBLE, i*ncols + j + 1, i*ncols + j + 1, MPI_COMM_WORLD);
           numsent++;
         }
       }
       cur_row = 0;
-      cur_col = 0;
+      cur_col = numprocs - 2;
       for (i = 0; i < nrows * ncols; i++) {
         MPI_Recv(&ans, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         sender = status.MPI_SOURCE;
@@ -76,8 +76,8 @@ int main(int argc, char* argv[])
             cur_col++;
           }
           for (j = 0; j < nrows; j++)
-            buffer[j] = bb[cur_col + cur_row*nrows];
-          MPI_Send(buffer, nrows, MPI_DOUBLE, cur_row*ncols + cur_col, cur_row*ncols + cur_col, MPI_COMM_WORLD);
+            buffer[j] = bb[cur_col + j*nrows];
+          MPI_Send(buffer, nrows, MPI_DOUBLE, sender, numsent + 1, MPI_COMM_WORLD);
           numsent++;
         } else {
           MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
@@ -90,16 +90,17 @@ int main(int argc, char* argv[])
       compare_matrices(cc2, cc1, nrows, nrows);
     } else {
       // Slave Code goes here
+      a = malloc(sizeof(double) * ncols);
       MPI_Bcast(a, ncols, MPI_DOUBLE, master, MPI_COMM_WORLD);
       if (myid <= nrows * ncols) {
         while (1) {
-          MPI_Recv(buffer, ncols, MPI_DOUBLE, master, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+          MPI_Recv(buffer, nrows, MPI_DOUBLE, master, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
           if (status.MPI_TAG == 0)
             break;
           entry = status.MPI_TAG;
           ans = 0.0;
-          for (j = 0; j < ncols; j++)
-            ans += a[j] * buffer[j];
+          for (i = 0; i < ncols; i++)
+            ans += a[i] * buffer[i];
           MPI_Send(&ans, 1, MPI_DOUBLE, master, entry, MPI_COMM_WORLD);
         }
       }
